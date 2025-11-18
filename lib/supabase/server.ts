f70aba60bce1ba987e97953a4a110babd266b520
JeiferMarrugo <jeifermarrugo24@gmail.com>
2025-11-18 02:38:51 -0500

    cambios en serve.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -7,8 +7,8 @@ export async function createClient() {
   console.log("🍪 Cookies detectadas en el server:", cookieStore.getAll())
 
   return createServerClient(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
